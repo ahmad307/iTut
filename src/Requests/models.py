889b9cbf82b884d37a01ab11e86dd26e626f4120
Ahmad <ahmad.hussain.307@gmail.com>
2018-09-16 03:40:03 +0200

    Removed unwanted files

--- src/Requests/models.py
+++ src/Requests/models.py
@@ -3,13 +3,8 @@ from firebase_admin import credentials
 from firebase_admin import db
 import datetime
 
-# Authentication & Initialization
-'''cred = credentials.Certificate('D:/Projects/itut-1-firebase-adminsdk-w95j8-16c10afd42.json')
-firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://itut-1.firebaseio.com'
-})'''
-ref = db.reference()
 
+ref = db.reference()
 
 def request(request):
     ref_requests = ref.child('requests')
